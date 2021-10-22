5487cf53f62b51e1c017b91f95902a3d7a91c654
ujjwal das <dasujjwal1996@outlook.com>
2021-10-22 22:59:44 +0530

    third commit

--- src/test/java/com/example/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/springjenkins/SpringJenkinsApplicationTests.java
@@ -1,20 +1,14 @@
 package com.example.springjenkins;
 
-
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringJenkinsApplicationTests {
-	
+class SpringDockerApplicationTests {
+
 	@Test
 	void contextLoads() {
-		
-		
 	}
 
 }
+
